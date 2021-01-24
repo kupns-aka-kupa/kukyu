7305bd6f34739f402af0eb19b89e334b28241736
kupns <kupns@mail.ru>
2021-01-24 17:28:26 +0300

    add tests template

--- kyu/_1kyu/loopover.py
+++ kyu/_1kyu/loopover.py
@@ -1,16 +1,2 @@
-grid5x5 = ['ABCDE',
-           'FGHIJ',
-           'KLMNO',
-           'PQRST',
-           'UVWXY']
-grid2x5 = ['ABCDE',
-           'FGHIJ']
-grid3x3 = ['ABC',
-           'DEF',
-           'GHJ']
-grid2x2 = ['AB',
-           'CD']
-
-
-def solve(s):
-    return
+def loopover(mixed_up_board, solved_board):
+    return []
--- kyu/test/test_1kyu.py
+++ kyu/test/test_1kyu.py
@@ -1,10 +1,61 @@
 import unittest
-from .._1kyu.loopover import *
+from kyu._1kyu.loopover import loopover
 
 
-class MyTestCase(unittest.TestCase):
-    def test_solving(self):
-        pass
+def board(s):
+    return [list(row) for row in s.split('\n')]
+
+
+def run_test(start, end):
+    return loopover(board(start), board(end))
+
+
+class LoopoverTestCase(unittest.TestCase):
+
+    def test_2x2_1(self):
+        """Test 2x2 (1)"""
+        m = run_test('12\n34', '12\n34')
+        self.assertIsNotNone(m)
+
+    def test_2x2_1(self):
+        """Test 2x2 (2)"""
+        m = run_test('42\n31', '12\n34')
+        self.assertIsNotNone(m)
+
+    @unittest.skip('Test 4x5')
+    def test_4x5_1(self):
+        m = run_test('ACDBE\nFGHIJ\nKLMNO\nPQRST',
+                     'ABCDE\nFGHIJ\nKLMNO\nPQRST')
+        self.assertIsNotNone(m)
+
+    @unittest.skip('Test 5x5 (1)')
+    def test_5x5_1(self):
+        m = run_test('ACDBE\nFGHIJ\nKLMNO\nPQRST\nUVWXY',
+                     'ABCDE\nFGHIJ\nKLMNO\nPQRST\nUVWXY')
+        self.assertIsNotNone(m)
+
+    @unittest.skip('Test 5x5 (2)')
+    def test_5x5_2(self):
+        m = run_test('ABCDE\nKGHIJ\nPLMNO\nFQRST\nUVWXY',
+                     'ABCDE\nFGHIJ\nKLMNO\nPQRST\nUVWXY')
+        self.assertIsNotNone(m)
+
+    @unittest.skip('Test 5x5 (3)')
+    def test_5x5_3(self):
+        m = run_test('CWMFJ\nORDBA\nNKGLY\nPHSVE\nXTQUI',
+                     'ABCDE\nFGHIJ\nKLMNO\nPQRST\nUVWXY')
+
+    @unittest.skip('Test 5x5 (unsolvable)')
+    def test_5x5_4(self):
+        m = run_test('WCMDJ\nORFBA\nKNGLY\nPHVSE\nTXQUI',
+                     'ABCDE\nFGHIJ\nKLMNO\nPQRST\nUVWXY')
+        self.assertIsNone(m)
+
+    @unittest.skip('Test 6x6')
+    def test_6x6(self):
+        m = run_test('WCMDJ0\nORFBA1\nKNGLY2\nPHVSE3\nTXQUI4\nZ56789',
+                     'ABCDEF\nGHIJKL\nMNOPQR\nSTUVWX\nYZ0123\n456789')
+        self.assertIsNotNone(m)
 
 
 if __name__ == '__main__':
