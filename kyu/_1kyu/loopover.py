3e78f453a3ae00f8ffcd5c560ff375f2ce515893
kupns <kupns@mail.ru>
2021-02-13 19:38:12 +0300

    add dfs

--- kyu/_1kyu/loopover.py
+++ kyu/_1kyu/loopover.py
@@ -1,14 +1,20 @@
-from numpy import roll, where, array, nditer
+from numpy import roll, where, array, nditer, vectorize
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import depth_first_tree
+
+to_char = vectorize(ord)
 
 
 def loopover(mixed, solved):
-    return solve(array(mixed), array(solved))
+    return solve(to_char(array(mixed)), to_char(array(solved)))
 
 
 def solve(mixed, solved):
+    matrix = csr_matrix(mixed)
+    print(matrix)
     for item in nditer(solved):
         for i, j in zip(*where(mixed == item)):
-            print(i, j)
+            pass
 
     print(mixed)
     return []
@@ -16,18 +22,22 @@ def solve(mixed, solved):
 
 def right(a, j):
     horizontal(a, j + 1, 1)
+    return f'R{j}'
 
 
 def left(a, j):
     horizontal(a, j + 1, -1)
+    return f'L{j}'
 
 
 def up(a, i):
     vertical(a, i + 1, -1)
+    return f'U{i}'
 
 
 def down(a, i):
     vertical(a, i + 1, 1)
+    return f'D{i}'
 
 
 def horizontal(a, j, d):
