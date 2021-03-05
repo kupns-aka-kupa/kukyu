e7deb0585a003784d1d45bb09a7a9dc3d3bd7f9e
nkupreenkov <nkupreenkov@htsts.ru>
2021-03-05 16:05:10 +0300

    refactor solve

--- kyu/_1kyu/loopover.py
+++ kyu/_1kyu/loopover.py
@@ -1,18 +1,7 @@
-from numpy import roll, where, array, nditer, vectorize, sign, subtract
-from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import depth_first_tree
+from numpy import roll, where, array, nditer, sign, subtract as sub, in1d
+from numpy.ma import masked_array
 
-to_char = vectorize(ord)
-
-path = []
 move_format = '{0}{1}'
-
-
-def extend_path(func):
-    def wrapper(*args):
-        path.append(func(*args))
-
-    return wrapper
 
 
 def loopover(mixed, solved):
@@ -23,69 +12,83 @@ class Solver:
     def __init__(self, mixed, solved):
         self.mixed = mixed
         self.solved = solved
-        self.x, self.y = subtract(array(mixed.shape), 1)
+        self.x, self.y = sub(array(mixed.shape), 1)
 
     def solve(self):
         sub_matrix = self.solved[:self.x, :self.x]
         with nditer(sub_matrix, flags=['multi_index']) as it:
             for item in it:
-                pull(self.mixed, item, it.multi_index[0])
-                push(self.mixed, item, it.multi_index[0])
+                yield from self.pull(item, it.multi_index[0])
+                yield from self.push(item, it.multi_index[0])
 
-        self.last_row()
-        self.last_column()
+        yield from self.last_row()
+        yield from self.last_column()
 
-        return path
+        print(self.mixed)
 
     def last_row(self):
         mask = self.solved[self.x, :self.x]
+        sliced = self.mixed[self.x, self.x - 1::-1]
 
-        for item in self.mixed[self.x, self.x - 1::-1]:
-            if item not in mask:
-                continue
+        for item in masked_array(sliced, ~in1d(sliced, mask)).compressed():
             for _, column in zip(*where(self.mixed == item)):
                 while self.mixed[self.x, self.x] in mask:
-                    up(self.mixed, self.x)
+                    yield self.up(self.x)
 
-                move(self.mixed, self.x, self.x - column, horizontal)
+                yield from self.move(self.x, self.x - column, self.horizontal)
 
         for item in self.solved[self.x, self.x - 1::-1]:
-            for row, column in zip(*where(self.mixed == item)):
-                move(self.mixed, self.x, self.x - row, vertical)
-                right(self.mixed, self.x)
+            for row, _ in zip(*where(self.mixed == item)):
+                yield from self.move(self.x, self.x - row, self.vertical)
+                yield self.right(self.x)
 
     def last_column(self):
         m = self.solved[:self.x + 1, self.x]
         for i in range(1, self.x):
-            for row, column in zip(*where(self.mixed == m[i])):
-                move(self.mixed, self.x, self.x - row, vertical)
-                right(self.mixed, self.x)
+            for row, _ in zip(*where(self.mixed == m[i])):
+                yield from self.move(self.x, self.x - row, self.vertical)
+                yield self.right(self.x)
 
                 for r, _ in zip(*where(self.mixed == m[i - 1])):
-                    move(self.mixed, self.x, self.x - r - 1, vertical)
+                    yield from self.move(self.x, self.x - r - 1, self.vertical)
 
-                left(self.mixed, self.x)
-        up(self.mixed, self.x)
+                yield self.left(self.x)
+        yield self.up(self.x)
 
+    def push(self, item, row):
+        for i, column in zip(*where(self.mixed == item)):
+            yield from self.move(i, self.x - column, self.horizontal)
+            yield from self.move(self.x, row - i, self.vertical)
+            yield self.left(row)
 
-def push(a, item, i):
-    for row, column in zip(*where(a == item)):
-        move(a, row, len(a) - column - 1, horizontal)
-        move(a, len(a) - 1, i - row, vertical)
-        left(a, i)
+    def pull(self, item, i):
+        for row, column in zip(*where(self.mixed == item)):
+            if i == row:
+                yield self.down(column)
+                yield self.right(row + 1)
+                yield self.up(column)
 
+    def right(self, row):
+        return self.horizontal(row + 1, 1)
 
-def pull(a, item, i):
-    for row, column in zip(*where(a == item)):
-        if i == row:
-            down(a, column)
-            right(a, row + 1)
-            up(a, column)
+    def left(self, row):
+        return self.horizontal(row + 1, -1)
 
+    def up(self, column):
+        return self.vertical(column + 1, -1)
 
-def move(a, index, steps, func):
-    for _ in range(abs(steps)):
-        func(a, index + 1, sign(steps))
+    def down(self, column):
+        return self.vertical(column + 1, 1)
+
+    def horizontal(self, r, d):
+        return horizontal(self.mixed, r, d)
+
+    def vertical(self, c, d):
+        return vertical(self.mixed, c, d)
+
+    def move(self, index, steps, func):
+        for _ in range(abs(steps)):
+            yield func(index + 1, sign(steps))
 
 
 def right(a, row):
@@ -104,7 +107,6 @@ def down(a, column):
     return vertical(a, column + 1, 1)
 
 
-@extend_path
 def horizontal(a, r, d):
     """
     @param r: row
@@ -115,7 +117,6 @@ def horizontal(a, r, d):
     return move_format.format('R' if d > 0 else 'L', abs(d))
 
 
-@extend_path
 def vertical(a, c, d):
     """
     @param c: column
